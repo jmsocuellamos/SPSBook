794e4a351642c64d22503496302a9652e4ebf8e7
Javier Morales Socuellamos <jm.socuellamos@gmail.com>
2022-02-08 09:25:34 +0100

    Add files via upload

---
+++ _common.R
@@ -0,0 +1,25 @@
+# example R options set globally
+options(width = 60)
+
+set.seed(1014)
+
+# example chunk options set globally
+knitr::opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE,
+  # cache = TRUE,
+  out.width = "70%",
+  fig.align = 'center',
+  fig.width = 6,
+  fig.asp = 0.618,  # 1 / phi
+  fig.show = "hold"
+)
+
+options(dplyr.print_min = 6, dplyr.print_max = 6)
+
+# Activate crayon output
+options(
+  crayon.enabled = TRUE,
+  pillar.bold = TRUE,
+  stringr.html = FALSE
+)
